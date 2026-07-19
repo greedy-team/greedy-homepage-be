24ed3d3c79a39e89a67802a35be87eb22bf1720b
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2026-07-20 06:48:06 +0900

    Refactor : 커스텀 예외 클래스명 변경

--- src/main/java/com/greedy/homepage/common/exception/HomepageException.java
+++ src/main/java/com/greedy/homepage/common/exception/HomepageException.java
@@ -3,16 +3,16 @@ package com.greedy.homepage.common.exception;
 import lombok.Getter;
 
 @Getter
-public class Exception extends RuntimeException {
+public class HomepageException extends RuntimeException {
 
     private final FailMessage failMessage;
 
-    public Exception(final FailMessage failMessage) {
+    public HomepageException(final FailMessage failMessage) {
         super(failMessage.getMessage());
         this.failMessage = failMessage;
     }
 
-    public Exception(final FailMessage failMessage, final Throwable cause) {
+    public HomepageException(final FailMessage failMessage, final Throwable cause) {
         super(failMessage.getMessage(), cause);
         this.failMessage = failMessage;
     }
