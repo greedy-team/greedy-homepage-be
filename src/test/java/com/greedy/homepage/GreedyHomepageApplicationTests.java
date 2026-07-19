0610f2eb02a40968a64c82b0e8d53f259e36da05
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2026-07-20 03:18:03 +0900

    Chore : spring 초기 세팅

---
+++ src/test/java/com/greedy/homepage/GreedyHomepageApplicationTests.java
@@ -0,0 +1,12 @@
+package com.greedy.homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreedyHomepageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
