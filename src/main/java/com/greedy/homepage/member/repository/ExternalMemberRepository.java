875e08baa525825425703c33430d2edf5dad5fdb
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2026-07-20 17:51:12 +0900

    Refactor : ExternalMember 패키지 이동

--- src/main/java/com/greedy/homepage/member/repository/ExternalMemberRepository.java
+++ src/main/java/com/greedy/homepage/member/repository/ExternalMemberRepository.java
@@ -1,6 +1,6 @@
-package com.greedy.homepage.externalmember.repository;
+package com.greedy.homepage.member.repository;
 
-import com.greedy.homepage.externalmember.domain.ExternalMember;
+import com.greedy.homepage.member.domain.ExternalMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExternalMemberRepository extends JpaRepository<ExternalMember, Long> {
