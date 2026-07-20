4ea5df6a63fe50fbd56d17238848ea9b6c1472a6
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2026-07-20 17:53:35 +0900

    Refactor : MemberActivity → MemberAction 이름 변경

---
+++ src/main/java/com/greedy/homepage/member/repository/MemberActionRepository.java
@@ -0,0 +1,7 @@
+package com.greedy.homepage.member.repository;
+
+import com.greedy.homepage.member.domain.MemberAction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberActionRepository extends JpaRepository<MemberAction, Long> {
+}
--- src/main/java/com/greedy/homepage/member/repository/MemberActivityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.greedy.homepage.member.repository;
-
-import com.greedy.homepage.member.domain.MemberActivity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberActivityRepository extends JpaRepository<MemberActivity, Long> {
-}
